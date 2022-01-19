54d54922008a3252b1240dd25ea1ae5fd08f6419
gilvaju <gcostajr@gmail.com>
2022-01-19 11:44:00 -0300

    feat: configurando tailwind para o storybook da lib components

---
+++ libs/components/tailwind.config.js
@@ -0,0 +1,16 @@
+const {join} = require("path");
+// const {createGlobPatternsForDependencies} = require("@nrwl/react/tailwind");
+
+module.exports = {
+  // presets: [
+  //   require('../../tailwind-workspace-preset.js')
+  // ],
+  content: [
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+    // ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
