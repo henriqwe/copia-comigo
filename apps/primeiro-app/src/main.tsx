08ccf9e8a6b5baf4df04806d8e91f8a39883e434
gilvaju <gcostajr@gmail.com>
2022-01-19 10:31:09 -0300

    feat: adicionando tailwind ao primeiro-app

--- apps/primeiro-app/src/main.tsx
+++ apps/primeiro-app/src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 
+import './styles.css'
+
 import App from './components/App/app';
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <div className={"bg-gray-500 text-white p-10"}>
+      <App />
+    </div>
   </StrictMode>,
   document.getElementById('root')
 );
---
+++ apps/primeiro-app/tailwind.config.js
@@ -0,0 +1,10 @@
+const {join} = require("path");
+module.exports = {
+  content: [
+    join(__dirname, './src/**/*.{js,ts,jsx,tsx}')
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
