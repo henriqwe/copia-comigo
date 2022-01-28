fd36c426a6dfa0ea3a302815e2a1aca2430e90d5
Alex <alexfstos@gmail.com>
2022-01-28 10:28:37 -0300

    refactor: removendo tests e stories da lib templates

--- libs/ui/templates/src/index.ts
+++ libs/ui/templates/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/templates';
+export * as Authentication from './lib/Authentication';
+export * as Base from './lib/Base';
+export * as FormAndTabs from './lib/FormAndTabs';
+export * as InternalNavigationAndSlide from './lib/InternalNavigationAndSlide';
