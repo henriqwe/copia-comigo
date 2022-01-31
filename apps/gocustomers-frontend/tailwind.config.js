73a2c61dd5dec4c8a001fc5c9a360df5c99efbd6
gilvaju <gcostajr@gmail.com>
2022-01-31 08:36:26 -0300

    refactor:  configuração do tailwind no projeto gocustomers

--- apps/gocustomers-frontend/tailwind.config.js
+++ apps/gocustomers-frontend/tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
     ],
     content: [
         join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
-        ...createGlobPatternsForDependencies(__dirname)
+        join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+        join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+        join(__dirname, '../../libs/ui/common/src/**/*.{js,ts,jsx,tsx}'),
+        join(__dirname, '../../libs/ui/blocks/src/**/*.{js,ts,jsx,tsx}'),
+        join(__dirname, '../../libs/ui/templates/src/**/*.{js,ts,jsx,tsx}'),
+        // ...createGlobPatternsForDependencies(join(__dirname, 'libs/ui-blocks')),
+        // ...createGlobPatternsForDependencies(join(__dirname, 'libs/ui-common')),
+        // ...createGlobPatternsForDependencies(join(__dirname, 'libs/ui-templates'))
     ],
     darkMode: false,
     theme: {
