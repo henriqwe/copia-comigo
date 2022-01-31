0c5e0c524ae9dfc4aae76aa6195a15aaf5c6960e
gilvaju <gcostajr@gmail.com>
2022-01-31 09:40:41 -0300

    refactor: movendo projeto gocustomers-frontend

---
+++ apps/gocustomers/cliente-web/src/components/domains/monitoring/Localization/SlidePanel/index.tsx
@@ -0,0 +1,17 @@
+import * as blocks from '@comigo/ui-blocks';
+import * as localizations from '../index';
+
+export default function SlidePanel() {
+  const { slidePanelState, setSlidePanelState } =
+    localizations.useLocalization();
+  return (
+    <blocks.Modal
+      title={'Histórico'}
+      open={slidePanelState.open}
+      handler={setSlidePanelState}
+      formContent={<localizations.HistoricLocalization />}
+      chevronDoubleRightIcon
+      noOverlay
+    />
+  );
+}
