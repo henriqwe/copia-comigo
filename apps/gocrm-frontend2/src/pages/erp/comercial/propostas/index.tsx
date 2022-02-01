3fdfac8c30f9a9433a671bdd24c7613ad8127814
Alex <alexfstos@gmail.com>
2022-02-01 14:43:58 -0300

    refactor: descomentando paginas do gocrm2

--- apps/gocrm-frontend2/src/pages/erp/comercial/propostas/index.tsx
+++ apps/gocrm-frontend2/src/pages/erp/comercial/propostas/index.tsx
@@ -1,39 +1,37 @@
-// import * as propostas from '&test/components/domains/erp/commercial/Proposals'
+import * as propostas from '&test/components/domains/erp/commercial/Proposals'
 
-// import rotas from '&test/components/domains/routes'
-// import InternalNavigationAndSlide from '&test/components/templates/InternalNavigationAndSlide'
+import rotas from '&test/components/domains/routes'
+import InternalNavigationAndSlide from '&test/components/templates/InternalNavigationAndSlide'
 
 export default function Propostas() {
   return (
-    <div/>
-    // <propostas.ListProvider>
-    //   <Page />
-    // </propostas.ListProvider>
+    <propostas.ListProvider>
+      <Page />
+    </propostas.ListProvider>
   )
 }
 
-// export function Page() {
-//   const { proposalsRefetch, proposalsLoading } = propostas.useList()
-//   const refetch = () => {
-//     proposalsRefetch()
-//   }
-//   const { usuario } = useUsuario()
-//   return (
+export function Page() {
+  const { proposalsRefetch, proposalsLoading } = propostas.useList()
+  const refetch = () => {
+    proposalsRefetch()
+  }
+  return (
     
-//     <InternalNavigationAndSlide
-//       SubMenu={<propostas.InternalNavigation />}
-//       title="Propostas"
-//       reload={{ action: refetch, state: proposalsLoading }}
-//       currentLocation={[
-//         { title: 'Rastreamento', url: rotas.erp.home },
-//         { title: 'Comercial', url: rotas.erp.comercial.index },
-//         {
-//           title: 'Propostas',
-//           url: rotas.erp.comercial.propostas.index
-//         }
-//       ]}
-//     >
-//       <propostas.List />
-//     </InternalNavigationAndSlide>
-//   )
-// }
+    <InternalNavigationAndSlide
+      SubMenu={<propostas.InternalNavigation />}
+      title="Propostas"
+      reload={{ action: refetch, state: proposalsLoading }}
+      currentLocation={[
+        { title: 'Rastreamento', url: rotas.erp.home },
+        { title: 'Comercial', url: rotas.erp.comercial.index },
+        {
+          title: 'Propostas',
+          url: rotas.erp.comercial.propostas.index
+        }
+      ]}
+    >
+      <propostas.List />
+    </InternalNavigationAndSlide>
+  )
+}
